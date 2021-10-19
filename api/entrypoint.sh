866be4b35327e786991d6ccbe30774b9344bce63
mehedishafi <mehedishafi@surecash.net>
2021-10-19 16:05:00 +0600

    fix: media files not found from nginx

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -5,14 +5,14 @@ do
     echo "Waiting for server volume ..."
 done
 
-until ./manage.py migrate
+until python manage.py migrate
 do
     echo "Waiting for db to be ready ..."
     sleep 2
 done
 
-./manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 # you can use either. its no big deal
-# gunicorn api.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
-./manage.py runserver 0.0.0.0:8000
+gunicorn api.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
+# python manage.py runserver 0.0.0.0:8000
